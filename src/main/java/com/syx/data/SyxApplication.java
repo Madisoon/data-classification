5c756fff848cc2a63ea358ab972b9ff72f96e374
zhaogang <zgyxx091418@163.com>
2017-10-18 20:57:14 +0800

    Initial commit

---
+++ src/main/java/com/syx/data/SyxApplication.java
@@ -0,0 +1,12 @@
+package com.syx.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyxApplication.class, args);
+	}
+}
